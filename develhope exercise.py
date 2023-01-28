d46b7274c3dac130476cce7813caac634215df0d
Tarık Sülüç <115278706+TrkSlc@users.noreply.github.com>
2023-01-28 13:19:38 +0300

    Update develhope exercise.py

--- develhope exercise.py
+++ develhope exercise.py
@@ -1,6 +1,5 @@
 from airflow import DAG 
-from airflow.operators.bash_operator 
-import BashOperator 
+from airflow.operators.bash_operator import BashOperator 
 from datetime import datetime, timedelta
 
 default_dag_args = { 'start_date': datetime(2022, 1, 1), 'email_on_failure': False, 'email_on_retry': False, 'retries': 1, 'retry_delay': timedelta(minutes=5), 'project_id': 1 }
